70ca9a7aa9d52290477d78ffccdcf6a126ca0c1a
cw <userchenwei@gmail.com>
2019-07-15 16:00:52 +0800

    init Commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "FilesWatch"
+version := "0.1"
+scalaVersion := "2.12.8"
+
+libraryDependencies += "com.typesafe" % "config" % "1.3.4"
---
+++ src/main/scala/FilesWatch.scala
@@ -0,0 +1,130 @@
+import java.io.{BufferedInputStream, BufferedOutputStream, FileInputStream, FileOutputStream}
+import java.nio.file._
+import java.util.zip.{ZipEntry, ZipInputStream}
+
+import com.typesafe.config.{Config, ConfigFactory}
+
+object FilesWatch {
+  def main(args: Array[String]): Unit = {
+
+    val config: Config = ConfigFactory.load("application.conf")
+
+    watch(config)
+
+  }
+
+  /*
+  *
+  * watch 用来临视文件夹
+  * @config 用来读取配置文件
+  * */
+  def watch(config:Config):Unit = {
+
+    val watchService:WatchService
+    = FileSystems.getDefault.newWatchService
+
+    //val path = Paths.get("/home/cw/Work")
+    //val unzipPath = Paths.get("/home/cw/unzipFiles")
+    val path = Paths.get(config.getString("wei.source"))
+    val unzipPath = Paths.get(config.getString("wei.unzipDir"))
+    path.register(
+      watchService,
+      StandardWatchEventKinds.ENTRY_CREATE)
+
+    var key:WatchKey = watchService.take()
+
+    while (key != null){
+      key.pollEvents().forEach{ x =>
+
+        println(s"Event kind: ${x.kind} . File affected: ${x.context}")
+
+        Thread.sleep(config.getInt("wei.wait"))
+        unzipFiles(unzipPath, path.resolve(x.context.toString), config)
+        println("unzip End")
+
+      }
+      key.reset()
+      key = watchService.take()
+    }
+  }
+
+  def isZipFile(source: Path, config: Config):Boolean = {
+
+    val is = new FileInputStream(source.toFile)
+    val b = new Array[Byte](5)
+    is.read(b, 0, b.length)
+
+    if(convertBytesToHex(b.toSeq) ==  config.getString("wei.type")) true
+    else false
+
+  }
+
+  def convertBytesToHex(bytes: Seq[Byte]): String = {
+    val sb = new StringBuilder
+    for (b <- bytes) {
+      sb.append(String.format("%02x", Byte.box(b)))
+    }
+    sb.toString
+  }
+
+
+
+  def unzipFiles(outDir: Path, source: Path, config: Config):Unit = {
+
+    val buffer = new Array[Byte](1024)
+    val stream = new ZipInputStream(new BufferedInputStream(new FileInputStream(source.toFile)))
+    val entry = stream.getNextEntry
+    /*
+    *在做解压之前，需做好文件目录设置
+    * */
+    //if(source.getFileName.toString.matches("zip$")) println("zipFiles")
+
+    //println(source.getFileName)
+    //if (isZipFile(source, config)) {println("zip file")}
+
+
+
+    if (isZipFile(source, config)){
+      doReadZipFile(entry, outDir, buffer, stream)
+    }
+    else println("Not Zip files")
+
+    //else  println("Error")
+
+    //
+    stream.close()
+  }
+
+  def doReadZipFile(entry: ZipEntry, outDir: Path, buffer: Array[Byte], stream: ZipInputStream):Int = {
+    entry match {
+      case s:ZipEntry => {
+        println("Unzipping: " + s.getName)
+        val filePath = outDir.resolve(s.getName)
+        val bos = new BufferedOutputStream(new FileOutputStream(filePath.toFile), buffer.length)
+        val size: Int = stream.read(buffer)
+        doWriteUnzipFiles(size, bos, buffer, stream)
+        bos.flush()
+        doReadZipFile(stream.getNextEntry, outDir, buffer, stream)
+        /*
+        *  close every stream when call ends
+        * */
+        bos.close()
+        1
+      }
+      case _ => 0
+    }
+  }
+  def doWriteUnzipFiles(size: Int, bos: BufferedOutputStream, buffer: Array[Byte], stream: ZipInputStream):Int = {
+
+    size match {
+      case x if x != -1 => {
+        bos.write(buffer, 0, x)
+        doWriteUnzipFiles(stream.read(buffer), bos, buffer, stream)
+      }
+      case _ => 0
+    }
+
+  }
+
+
+}
